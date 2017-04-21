4678f9328fdccd28ca68bbf7571ab7df208b2199
Emile Loiselle <emile.loiselle@hotmail.com>
2017-04-21 11:56:22 -0400

    Majorité commentaires

--- SFML_Vroom/main.cpp
+++ SFML_Vroom/main.cpp
@@ -1,51 +1,55 @@
 
 #include <SFML/Graphics.hpp>
-#include <iostream>
-#include <math.h>
-using namespace std;
-using namespace sf;
+using namespace sf;	//SFML
 
-#define _USE_MATH_DEFINES
-
+//Constantes
 const double PI = atan(1) * 4;
 
+//Prototypes
 double getRads(double degre);
 
 int main()
 {
+	//Initialisation SFML
 	RenderWindow window(VideoMode(800, 600), "Vroooom!!!");
-
-	double degre = 90;
-	double radian = 0;
-	double angleInc = 0.01;
-
 	Texture texture;
 	RenderTexture;
 	Sprite sprite;
-	if (!texture.loadFromFile("orange32x16.png", IntRect(0, 0, 32, 16)))
-	{
-		cout << "error loading";
-		system("pause>0");
-	}
+	Event event;
 
+	//Variables
+	double degree = 90;
+	double radian = 0;
+	double angleInc = 0.01;
+
+	//Chargement de la texture de l'auto
+	texture.loadFromFile("orange32x16.png", IntRect(0, 0, 32, 16));
+
+	//Insertion de la texture dans le sprite
 	sprite.setTexture(texture);
 
+	//Initialisation  auto
 	sprite.setPosition(100, 100);
-	sprite.rotate(degre);
+	sprite.rotate(degree);
 
+	//Tant que le jeu roule
 	while (window.isOpen())
 	{
-		Event event;
+		//Attend les événements
 		while (window.pollEvent(event))
 		{
+			//Si appuie sur X ou Alt+F4
 			if (event.type == Event::Closed)
+
+				//Ferme le jeu
 				window.close();
 		}
 
-		degre -= angleInc;
+		//Gère les virages
+		degree -= angleInc;
 		sprite.rotate(-angleInc);
 
-		sprite.move(cos(getRads(degre)) / 100, sin(getRads(degre)) / 100);
+		sprite.move(cos(getRads(degree)) / 100, sin(getRads(degree)) / 100);
 		window.clear();
 		window.draw(sprite);
 		window.display();
