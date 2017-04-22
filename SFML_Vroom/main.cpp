c2008541a9fd8de85940ba854586bb6ed6c20812
Emile Loiselle <emile.loiselle@hotmail.com>
2017-04-22 16:08:06 -0400

    Ajout d'un joueur  - Joueur Rouge = Flèches  - Joueur Bleu = WASD

--- SFML_Vroom/main.cpp
+++ SFML_Vroom/main.cpp
@@ -15,78 +15,118 @@ int main()
 {
 	//Initialisation SFML
 	RenderWindow window(VideoMode(1280, 720), "Vroooom!!!");
-	Texture texture;
+	Texture texture[4];
 	RenderTexture;
-	Sprite sprite;
+	Sprite sprJoueur[4];
+	Sprite sprJoueur2;
 	Event event;
 	Clock horlogeDelta;			//https://en.wikipedia.org/wiki/%CE%94T
 
+	//Chargement de la texture de l'auto 1
+	texture[1].loadFromFile("orange32x16.png", IntRect(0, 0, 32, 16));
+	//Chargement de la texture de l'auto 2
+	texture[2].loadFromFile("blue32x16.png", IntRect(0, 0, 32, 16));
+
 	//Variables
-	automobile joueur;
+	automobile joueurs[4];
 
 	//Limite le nombre d'images par secondes
 	window.setFramerateLimit(60);
 
-	//Chargement de la texture de l'auto
-	texture.loadFromFile("orange32x16.png", IntRect(0, 0, 32, 16));
+	int choix = 2;
 
-	//Insertion de la texture dans le sprite et changement du point d'origine
-	sprite.setTexture(texture);
-	sprite.setOrigin(12, 8);
+	for (int i = choix; i > 0; i--)
+	{
+		//Insertion de la texture dans le sprite et changement du point d'origine
+		sprJoueur[i].setTexture(texture[i]);
+		sprJoueur[i].setOrigin(12, 8);
 
-	//Initialisation auto
-	sprite.setPosition(100, 100);
-	sprite.rotate(joueur.getDegre());
+		//Initialisation auto
+		sprJoueur[i].setPosition(100 * i, 100);
+		sprJoueur[i].rotate(joueurs[i].getDegre());
+	}
 
 	//Tant que le jeu roule
 	while (window.isOpen())
 	{
+		//Efface l'affichage précédent
+		window.clear();
+
 		//Réinitialise l'horloge
 		Time tempsDelta = horlogeDelta.restart();
 
 		//Attend les événements
 		while (window.pollEvent(event))
-			if(event.type == sf::Event::Closed || ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)))
+			if (event.type == sf::Event::Closed || ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)))
 				window.close();
 
-		//Gauche
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-			joueur.setVirage(1);
+		for (int i = choix; i > 0; i--)
+		{
+			if (i == 1)
+			{
+				//Gauche
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+					joueurs[i].setVirage(1);
 
-		//Droite
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-			joueur.setVirage(2);
+				//Droite
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+					joueurs[i].setVirage(2);
 
-		//Nulle
-		else
-			joueur.setVirage(0);
+				//Nulle
+				else
+					joueurs[i].setVirage(0);
 
-		//Haut
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-			joueur.effectuerVelocite(1, tempsDelta.asMilliseconds());
+				//Haut
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+					joueurs[i].effectuerVelocite(1, tempsDelta.asMilliseconds());
 
-		//Bas
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-			joueur.effectuerVelocite(0, tempsDelta.asMilliseconds());
+				//Bas
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+					joueurs[i].effectuerVelocite(0, tempsDelta.asMilliseconds());
+			}
 
-		//Gère les virages
-		joueur.effectuerVirage();
-		sprite.setRotation(joueur.getDegre());
-		joueur.setVirage(0);
+			else if (i == 2)
+			{
+				//Gauche
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+					joueurs[i].setVirage(1);
 
-		//Gère le mouvement du véhicule
-		sprite.move(
-			joueur.getVelociteX() * cos(
-				joueur.convertDegreeRadian(joueur.getDegre())) * tempsDelta.asSeconds() , 
-			joueur.getVelociteY() * sin(
-				joueur.convertDegreeRadian(joueur.getDegre())) * tempsDelta.asSeconds());
+				//Droite
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+					joueurs[i].setVirage(2);
 
-		//Affaiblissement de la vélocité
-		joueur.velociteAffaiblir();
+				//Nulle
+				else
+					joueurs[i].setVirage(0);
 
-		//Gère l'affichage
-		window.clear();
-		window.draw(sprite);
+				//Haut
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+					joueurs[i].effectuerVelocite(1, tempsDelta.asMilliseconds());
+
+				//Bas
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+					joueurs[i].effectuerVelocite(0, tempsDelta.asMilliseconds());
+			}
+
+			//Gère les virages
+			joueurs[i].effectuerVirage();
+			sprJoueur[i].setRotation(joueurs[i].getDegre());
+			joueurs[i].setVirage(0);
+
+			//Gère le mouvement du véhicule
+			sprJoueur[i].move(
+				joueurs[i].getVelociteX() * cos(
+					joueurs[i].convertDegreeRadian(joueurs[i].getDegre())) * tempsDelta.asSeconds(),
+				joueurs[i].getVelociteY() * sin(
+					joueurs[i].convertDegreeRadian(joueurs[i].getDegre())) * tempsDelta.asSeconds());
+
+			//Affaiblissement de la vélocité
+			joueurs[i].velociteAffaiblir();
+
+			window.draw(sprJoueur[i]);
+		}
+
+		//Rafraîchi l'affichage
 		window.display();
 	}
 	return 0;
